d4581ec9132e3922f49eb799e87af87d57aad08c
Anthony Chinwe <chinwe221@gmail.com>
2021-10-13 04:04:00 -0700

    first commit

---
+++ src/test/java/com/phi/studentserviceapigateway/StudentServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phi.studentserviceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
